e85fb848506795a14a19fbed6d8a5f31dc4e9091
maxswin99 <andreaschristianfirga06@gmail.com>
2025-03-17 01:25:11 +0700

    Pesan commit yang menjelaskan perubahan

--- tpmodul5_103022300002/Program.cs
+++ tpmodul5_103022300002/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class HaloGeneric
+{
+    public void SapaUser<T>(T data)
+    {
+        Console.WriteLine("Halo " + data);
+    }
+}
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Aloy");
+        halo.SapaUser("Atong");
+
+        DataGeneric<string> dataNim = new DataGeneric<string>("103022300003");
+        dataNim.PrintData();
+
+        DataGeneric<long> dataNimInt = new DataGeneric<long>(103022300090);
+        dataNimInt.PrintData();
+    }
+}
