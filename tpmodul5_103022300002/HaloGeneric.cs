884abb60be988cfee8dad4da7d83a54ff5a7f2ff
maxswin99 <andreaschristianfirga06@gmail.com>
2025-03-17 00:11:40 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_103022300002/HaloGeneric.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodule5_103022300002  // Sesuaikan dengan namespace project
+{
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+}
--- tpmodul5_103022300002/Program.cs
+++ tpmodul5_103022300002/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodule5_103022300002; 
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Praktikan A");
+        halo.SapaUser("123");
+    }
+}
